c6d8100360a79db8c5aed7606a2e9769a3c4bb72
Augusto Alexandre01 <augustoams01@gmail.com>
2024-09-26 12:59:43 -0300

    hero-conclusion

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,29 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import config from "../../tailwind.config";
+
 export const Header = () => {
   return(
 
-    <div className="flex justify-center items-center fixed top-3 w-full z-40">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">Inicio</a>
-        <a href="#" className="nav-item">Projetos</a>
-        <a href="#" className="nav-item">Sobre</a>
-        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contato</a>
+    <div className="flex justify-around items-center fixed top-3 w-full z-40">
+      
+      <div className="w-[100px] lg:block md:block sm:hidden">
+        <h1 className="text-2xl font-semibold font-serif">Alex.</h1>
+      </div>
+      
+      <div className="flex items-center justify-center">
+        <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+          <a href="#" className="nav-item">Inicio</a>
+          <a href="#" className="nav-item">Projetos</a>
+          <a href="#" className="nav-item">Sobre</a>
+          <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contato</a>
+        </nav>
+      </div>
+
+         
+      <nav className="flex gap-3 p-0.5 lg:flex md:flex  sm:hidden">
+        <a href="#" className=""><FaGithub size={25}/></a>
+        <a href="#" className=""><FaInstagram size={25}/></a>
+        <a href="#" className=""><FaLinkedin size={25}/></a>
       </nav>
     </div>
       
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,29 +3,71 @@ import Image from 'next/image';
 import grainImage from '@/assets/images/grain.jpg';
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from '@/assets/icons/sparkle.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
 
 export const HeroSection = () => {
   return (
   <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
 
-    <div className='absolute inset-0 -z-30 opacity-30 bg-cover' style={{backgroundImage: `url(${grainImage.src})`}}></div>
+    <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
 
-    <div className='hero-circle'></div>
-    <div className='hero-circle size-[900px]'></div>
-    <div className='hero-circle size-[1080px]'></div>
-    <div className='hero-circle size-[1220px]'></div>
+      <div className='absolute inset-0 -z-30 opacity-30 bg-cover' style={{backgroundImage: `url(${grainImage.src})`}}></div>
 
-    <HeroOrbit size={800} rotation={-72}>
-      <StarIcon className="size-28 text-emerald-300"/>
-    </HeroOrbit>
+      <div className='hero-circle'></div>
+      <div className='hero-circle size-[900px]'></div>
+      <div className='hero-circle size-[1080px]'></div>
+      <div className='hero-circle size-[1220px]'></div>
 
-    <div className="container">
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-sky-300"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-sky-300"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-8 text-sky-300"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={580} rotation={178}>
+        <SparkleIcon className="size-10 text-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={710} rotation={143}>
+        <SparkleIcon className="size-14 text-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={720} rotation={0}>
+        <div className="size-3 rounded-full bg-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={720} rotation={85}>
+        <div className="size-3 rounded-full bg-sky-300/20"/>
+      </HeroOrbit>
+
+      <HeroOrbit size={580} rotation={230}>
+        <div className="size-3 rounded-full bg-sky-300/20"/>
+      </HeroOrbit>
+
+    </div>
+    
+
+    <div className="container relative">
 
       <div className='flex flex-col items-center'>
         <Image src={memojiImage} className='size-[100px] lg:size-[120px] relative top-[10px]' alt='Person behind a leptop'/>
         <div className='bg-gray-950 border border-gray-800 items-center gap-4 px-4 py-1.5 inline-flex rounded-lg'>
-          <div className='bg-green-500 size-2.5 rounded-full '></div>
+          <div className='bg-sky-500 size-2.5 rounded-full '></div>
           <div className='text-sm font-medium'>Disponivel para novos projetos</div>
         </div>
       </div>

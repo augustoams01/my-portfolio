2e94ae72e4e2e18385cecc3b8c460718a071ef0c
augustoams01 <agustoams01@gmail.com>
2024-10-14 16:25:01 -0300

    Contact-section-commit

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,25 @@
+import ArrouUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+import BgImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="bg-gradient-to-r from-sky-400 to-emerald-300 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+          <div className="absolute inset-0 opacity-5 bg-cover -z-10"  style={{
+            backgroundImage: `url(${BgImage.src})`
+          }}></div> 
+          <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">Vamos criar algo incrível juntos!</h2>
+              <p className="text-sm mt-2 md:text-base">Preparado para dar vida ao seu novo projeto? vamos conversar e conectar nossas ideias para alcançar suas metas.</p>
+            </div>
+            <div>
+            <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900"><span className="font-semibold">Fale Comigo</span><ArrouUpRightIcon className="size-4"/></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
